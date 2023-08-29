7c027af171be813dbe118963184636a090c70290
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-08-29 17:20:05 +0530

    tanuja code

--- main.java
+++ main.java
@@ -1,5 +1,21 @@
+class main{
 // Sample java code
 
 // code to be written by tanuja
+static int factorial(int n){
+        int factorial = 1;
+        int i =1;
+        while (i <= n){
+            factorial*=i;
+            i++;
+        }
+        return factorial;
+    }
+    public static void main(String[] args) {
+        int n =7 , r = 3, permutation;
+        permutation = factorial(n) / factorial(n-r);
+        System.out.println("Permutation: "+permutation);
+    }
+}
 
 // Code to be written by P Mayur
