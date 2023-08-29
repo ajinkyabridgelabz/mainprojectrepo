f1b9c20e35c8e24a2b9dfced17c267a981218370
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-08-29 17:15:12 +0530

    this is code by mayur

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
 // Sample java code
+class Main {
+    public static void main(String[] args) {
+            Main.showMe("Mayur");
+    }
 
-// code to be written by tanuja
+    // code to be written by tanuja
 
-// Code to be written by P Mayur
+    // Code to be written by P Mayur
+
+    static String showMe(String name) {
+        return "Hiiii " + name;
+    }
+}
